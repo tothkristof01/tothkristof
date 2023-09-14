d416b7592345987474ffaccc38b005cdff09b499
tothkristof01 <tothkristofbence01@gmail.com>
2023-09-14 12:35:11 +0200

    szinkron

--- elsofile.py
+++ elsofile.py
@@ -1,2 +1,57 @@
 print("SHEEEEEEEEEEESHH")
 
+x=2
+x, y=5, "hat"
+
+print (x)
+print (y)
+szöveg="motorkerékpár"
+
+print (szöveg)
+
+szoveg="motorkerékpár"
+
+print (szoveg[2])
+
+print (szoveg[5:])
+print (szoveg[-3:])
+print (szoveg[5:10])
+
+
+lista=1,3,5,7,9
+
+print (lista)
+print (lista[1])
+
+c=2
+
+c+=1
+
+c*=2
+
+print (c)
+
+szoveg*=2
+
+print (szoveg)
+
+print ("Szia", "\nBéla!,", sep=" ")
+
+'''print ("Mizu?")
+Na ez
+naez
+
+'''
+
+print ('''Ez
+most
+több
+sor
+''')
+
+nev=input("Hogy hívnak?:")
+
+print(f'Szia {nev}')
+
+print("Hahó {0}!".format(nev))
+
